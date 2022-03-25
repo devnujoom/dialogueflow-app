35e68d86ed9d935a7378be321013af57662b7d62
shabi m f <shabi@adagency.design>
2022-03-25 13:01:34 +0530

    formatted with global variables

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const PORT = 3030;
+const dotenv = require('dotenv')
+dotenv.config()
+const PORT =  process.env.PORT || 3000;
 const app = express();
 
 app.listen(PORT, ()=>{
