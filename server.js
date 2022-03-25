32765074d2f6dc836b73e07b2bdcf8b0d70fe943
shabi m f <shabi@adagency.design>
2022-03-25 13:55:15 +0530

    updated modules and formnatted

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
-const dotenv = require('dotenv')
-dotenv.config()
+const path = require('path');
+const socketio = require('socket.io');
+const dotenv = require('dotenv');
+const colors = require('colors');
+const uuid = require('uuid');
+const dialogflow = require("@google-cloud/dialogflow");
+dotenv.config({ path: "./config/config.env" });
+
 const PORT =  process.env.PORT || 3000;
 const app = express();
 
 app.listen(PORT, ()=>{
-  console.log(`listening on Port:`+ PORT+` And URL: http://localhost:`+PORT);
+  console.log(`listening on Port:${PORT} And URL: http://localhost:${PORT}`.yellow.bold);
 });
